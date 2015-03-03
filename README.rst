47af1c4f844a181a49296afef241043835da51b5
Jiangge Zhang <tonyseek@gmail.com>
2015-03-03 18:26:21 +0800

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,9 @@
+html5lib-truncation
+===================
+
+::
+
+    import html5lib
+    import html5lib_truncation
+
+    etree = html5lib.parse(u'<p>A <a href="#">very very long link</a></p>')
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+with open('README.rst') as readme:
+    next(readme)
+    long_description = ''.join(readme).strip()
+
+
+setup(
+    name='html5lib-truncation',
+    version='0.1.0.dev0',
+    author='Jiangge Zhang',
+    author_email='tonyseek@gmail.com',
+    description='Truncating HTML with html5lib filter',
+    long_description=long_description,
+    url='https://github.com/tonyseek/html5lib-truncation',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    py_modules=['html5lib_truncation'],
+    platforms=['Any'],
+)
